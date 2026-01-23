30205f345789fb33a485330c8928ef5bb6b7a3f6
MarcG14 <marc.gouriou010@gmail.com>
2026-01-22 17:44:48 -0800

    Co-authored-by: cynthiazhang29 <cynthiazhang29@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,78 @@
+package frc.robot.subsystems;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
+
+public class Intake extends SubsystemBase {
+   private static Intake instance;
+   private static TalonFX motor;
+   
+   private Intake()
+   {
+        motor = new TalonFX(Constants.Intake.MOTOR_ID);
+        config();
+   }
+
+   private void config()
+   {
+        TalonFXConfiguration config = new TalonFXConfiguration();
+
+        config.Feedback.SensorToMechanismRatio = Constants.Intake.GEAR_RATIO;
+
+        config.MotorOutput.Inverted = Constants.Intake.INVERTED;
+        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+        config.Slot0.kP = Constants.Hood.KP;
+        config.Slot0.kI = Constants.Hood.KI;
+        config.Slot0.kD = Constants.Hood.KD;
+        config.Slot0.kG = Constants.Hood.KG;
+        config.Slot0.kV = Constants.Hood.KV;
+
+        config.Voltage.PeakForwardVoltage = Constants.MAX_VOLTAGE;
+        config.Voltage.PeakReverseVoltage = -Constants.MAX_VOLTAGE;
+
+        config.CurrentLimits.StatorCurrentLimit = Constants.Intake.STATOR_CURRENT_LIMIT;
+        config.CurrentLimits.StatorCurrentLimit = Constants.Intake.SUPPLY_CURRENT_LIMIT;
+
+        motor.getConfigurator().apply(config);
+   }
+   
+    public Voltage getVoltage()
+    {
+        return motor.getMotorVoltage().getValue();
+    }
+    
+    public AngularVelocity getVelocity()
+    {
+        return motor.getVelocity().getValue();
+    }
+   
+    public void setVoltage (Voltage voltage)
+    {
+        motor.setControl(new VoltageOut(voltage));
+    }
+
+    public void setPosition (Angle position)
+    {
+        motor.setPosition(position);
+    }
+
+    public void setDutyCycle(double velocity) {
+        motor.setControl(new DutyCycleOut(velocity));
+    }
+
+   public static Intake getInstance()
+   {
+        if(instance == null) instance = new Intake();
+        return instance;
+   }
+}
