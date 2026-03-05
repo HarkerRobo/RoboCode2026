b385ff3aeba145750dcd3c24f3d2927ab693af44
27elijahl <27elijahl@students.harker.org>
2026-03-05 11:46:30 -0800

    Revert "Remove Hood mapping"
    This reverts commit f7f5e80c7d125813ab795c89786c04077c2a9ae8.

--- src/main/java/frc/robot/commands/hood/AimToAngle.java
+++ src/main/java/frc/robot/commands/hood/AimToAngle.java
@@ -34,7 +34,7 @@ public class AimToAngle extends Command
     {
         pitch = pitcher.getAsDouble();
         
-        Hood.getInstance().moveToPosition(Degrees.of(pitch));
+        Hood.getInstance().moveToEffectivePosition(Degrees.of(pitch));
         System.out.println("Aiming: " + pitch + "°");
     }
 
